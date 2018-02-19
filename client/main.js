74578d83b1b1f4ae1e719ac307621f5c537a2387
Ola McDaniel <omcdaniel1@gmail.com>
2018-02-19 16:33:32 -0600

    Add Prototype class. Update some stuff. Messed around with Login.  Code is not at all standard or good, just there for now.

--- client/main.js
+++ client/main.js
@@ -1,1 +1,12 @@
+import { React } from 'react';
+import { Meteor } from 'meteor/meteor';
+import {render} from 'react-dom';
+
 import './../imports/html/verification/Login.html';
+
+import Prototype from  "../imports/js/app/Prototype.js";
+
+Meteor.startup(() => {
+    // render(<Prototype />,
+    //     document.getElementById('render-target'));
+});
