946d504f80d60206760c027d0f6c945a6805915c
Ola McDaniel <omcdaniel1@gmail.com>
2018-02-18 18:29:04 -0600

    Update folder structure to comply with meteor.

--- imports/js/verification/LoginView.js
+++ imports/js/verification/LoginView.js
@@ -1,7 +1,8 @@
-function check(form) {
-    if(form.userid.value === 'dpara' && form.pswrd.value == "123") {
+export let login;
+login = function check(form) {
+    if (form.userid.value === 'dpara' && form.pswrd.value === "123") {
         window.open('http://www.depaul.edu/')
     } else {
         alert("The username and password you entered did not match what is on file")
     }
-}
+};
