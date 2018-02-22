3dd295f84d5e84030e5baf13292d908b7e11fc75
David <davidjpara@gmail.com>
2018-02-21 21:19:44 -0600

    login layout in css, html, js native. Working to convert to React

--- imports/js/verification/LoginView.js
+++ imports/js/verification/LoginView.js
@@ -6,10 +6,38 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-function check(form) {
-    if (form.userid.value === 'dpara' && form.pswrd.value === "123") {
-        window.open('http://www.depaul.edu/')
-    } else {
-        alert("The username and password you entered did not match what is on file")
+import '../../css/verification/LoginSheet.css';
+
+class LoginComponent extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.check = this.check.bind(this);
     }
-};
+
+    check(form) {
+        if (form.userid.value === 'dpara' && form.pswrd.value === "123") {
+            window.open('http://www.depaul.edu/')
+        } else {
+            alert("The username and password you entered did not match what is on file")
+        }
+    }
+
+    render() {
+        return <form>
+            <input type="text" placeholder="username" name="userid"/>
+            <input type="password" placeholder="password" name="pswrd"/>
+            <input type="button" onclick={this.check} value="Login"/>
+        </form>;
+    }
+}
+
+ReactDom.render(LoginComponent);
+
+// function check(form) {
+//     if (form.userid.value === 'dpara' && form.pswrd.value === "123") {
+//         window.open('http://www.depaul.edu/')
+//     } else {
+//         alert("The username and password you entered did not match what is on file")
+//     }
+// }
